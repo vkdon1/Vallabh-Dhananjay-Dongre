be74e1f179a3775aa2f52d12082f0e6c12352803
vallabh Dongre <150423429+vkdon1@users.noreply.github.com>
2025-05-02 18:11:54 +0530

    Create Problem2.cpp

---
+++ Problem2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter value of a: ";
+    cin >> a;
+
+    for (int i = 0; i < a; i++) 
+    {
+        cout << 2 * i + 1;
+        if (i != a - 1)
+            cout << ", ";
+    }
+    cout << endl;
+    return 0;
+}
